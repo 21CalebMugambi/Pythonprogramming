4cd7000378394200c180cf705417e1cf2545600c
21CalebMugambi <clbmugambi@gmail.com>
2023-02-10 11:12:32 +0300

    Pythonprogramming

---
+++ list.py/range.py
@@ -0,0 +1,5 @@
+numbers=[74,3,21,1]
+for i in range(10):
+  numbers.append(i)
+for i in range(len(numbers)):
+  print(numbers[i])
--- list.py/slice.py
+++ list.py/slice.py
@@ -1,16 +1,16 @@
 #slicing a list
-numbers=(74,3,21,1)
+numbers=[74,3,21,1]
 print(numbers[2:])
 
 #a list of even numbers 
 even_num=[]
-for num in range (20):
-    if(num%2==0):
+for num in range(20):
+    if num%2==0:
         even_num.append(num)
 for i in range(len(even_num)):
     print(even_num[i])
 
 for i in range(10):
   numbers.append(i)
-  for i in range(len(even_num)):
-    print(numbers[i])
+for i in range(len(even_num)):
+  print(numbers[i])
